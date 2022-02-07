c70be5be521e1f20ac9dd38e0bc698e33ef35d3b
PestePrajit <99005631+PestePrajit@users.noreply.github.com>
2022-02-07 07:57:26 +0200

    Delete gigoloText.js

--- gigoloText.js
+++
@@ -1,2 +0,0 @@
-const gigoloText = "Sunt frumos, ce fata am de gigolo\nMor femeile pe mine, ce misto (ce misto!)\nNici una din sat mie nu mi-a scapat\nSi-acum sigur una mai reclamat\nBate-n noapte cineva\nCu parul la usa mea\nDaca e politia...\nCe ma fac ca iar ma ia\nTu la toate, tu la munca, tu la pat\nTu la mine-n sat nu e nici un barbat\nToate fug de acasa cum le-am invatat\nAoleu ce scandal am provocat\nBate-n noapte cineva\nCu parul la usa mea\nDaca e politia...\nCe ma fac ca iar ma ia";
-exports.gigoloText = gigoloText;
